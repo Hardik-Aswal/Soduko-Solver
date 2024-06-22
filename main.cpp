6cb7609759167d3caadd4a7fb663db7102d74ea4
hardik <137987162+Hardik-Aswal@users.noreply.github.com>
2024-06-22 19:39:00 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+void print(int arr[N][N])
+
+{
+
+     for (int i = 0; i < N; i++)
+
+      {
+
+         for (int j = 0; j < N; j++)
+
+            printf("%d ",arr[i][j]);
+
+         printf("\n");
+
+       }
+
+}
+
+int isSafe(int grid[N][N], int row, int col, int num)
+
+{
+
+    for (int x = 0; x <= 8; x++)
+
+        if (grid[row][x] == num)
+
+            return 0;
+
+    for (int x = 0; x <= 8; x++)
+
+        if (grid[x][col] == num)
+
+            return 0;
+
+    int startRow = row - row % 3,
+
+                 startCol = col - col % 3;
+
+   for (int i = 0; i < 3; i++)
+
+        for (int j = 0; j < 3; j++)
+
+            if (grid[i + startRow][j +
+
+                          startCol] == num)
+
+                return 0;
+
+      return 1;
+
+}
